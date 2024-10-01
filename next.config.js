9ae27626eb02f4aac5f81471924ebf8e0d10b28b
lesyladriano <lesyl_adriano@outlook.com>
2024-10-01 17:57:57 +0800

    npm update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // other configuration options...
-  // experimental: {
-  //   serverActions: true, // Remove this line
-  // },
+
+  images: {
+    formats: ['image/avif', 'image/webp'], // This enables support for modern image formats.
+  },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Ensure Sharp is used for image processing
+      require('sharp');
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
